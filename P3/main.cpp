30ec053a3bab9d05c31ac2b935644dd7d1e7099d
JacqueWinarto <36138165+JacqueWinarto@users.noreply.github.com>
2021-07-01 05:26:48 +0700

    Update main.cpp

--- P3/main.cpp
+++ P3/main.cpp
@@ -113,7 +113,7 @@ void DrawText(ShaderProgram *program, GLuint fontTextureID, std::string text, fl
 
 void Initialize() {
     SDL_Init(SDL_INIT_VIDEO);
-    displayWindow = SDL_CreateWindow("Textured!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
+    displayWindow = SDL_CreateWindow("Lunar Lander!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
     SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
     SDL_GL_MakeCurrent(displayWindow, context);
     
